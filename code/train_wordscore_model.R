fab47780213db592c4bc8424084d93c641708519
adamvig96 <vig1396@gmail.com>
2021-12-29 22:38:51 +0100

    solve version problem

--- code/train_wordscore_model.R
+++ code/train_wordscore_model.R
@@ -19,15 +19,9 @@ library(quanteda.textplots)
 library(tidyverse)
 library(gofastr)
 
-<<<<<<< HEAD
 speaker_names <- prep_stopwords(read_csv("data/input/representative_names_2014-2018.csv")$Név %>% tolower())
 
 parl_text <- read_csv("data/input/parlament_speech_2014-2018.csv")  %>% 
-=======
-names <- prep_stopwords(read_csv("data/input/representative_names_2018-2020.csv")$Név %>% tolower()) #%>% str_replace(" ","_")
-
-parl_text <- read_csv("data/input/parlament_speech_2018-2020.csv")  %>% 
->>>>>>> 80c4926aeb252dc88c571ade4a57fe998808b7ad
   filter(type == "vezérszónoki felszólalás" 
          | type == "felszólalás" 
          | type == "elhangzik az interpelláció/kérdés/azonnali kérdés"
@@ -55,6 +49,7 @@ parl_text %>% select("speaker_party") %>% table()
 
 # drop jobbik now
 parl_text <- parl_text %>% filter(speaker_party != "Jobbik")
+
 
 corpus <- corpus(parl_text %>% select(text))
 docvars(corpus, "speaker_party") <- parl_text$speaker_party
@@ -99,29 +94,12 @@ parl_tokens <- tokens(corpus,
 
 rm(parl_text)
 rm(corpus)
-<<<<<<< HEAD
 
-=======
->>>>>>> 80c4926aeb252dc88c571ade4a57fe998808b7ad
 
 #stemming
 parl_tokens <- parl_tokens %>% tokens_wordstem(language = 'hu')
 
-<<<<<<< HEAD
 # Scaling --------------------------------------------------------------
-=======
-# unigramm 
-
-dtm <- dfm(parl_tokens, groups = "side") %>%
-  dfm_trim(groups = "side", min_termfreq = 20) 
-
-unigram_keyness <- dtm %>% textstat_keyness(target=1, measure="chi2")
-
-unigram_keyness %>% write_csv("data/output/unigramms_1820.csv")
-
-textplot_wordcloud(dfm_group(dtm, 'side'), comparison = TRUE,max_words = 100)
-
->>>>>>> 80c4926aeb252dc88c571ade4a57fe998808b7ad
 
 # bigramm 
 toks_2gram <- tokens_ngrams(parl_tokens, n = 2)
@@ -132,17 +110,7 @@ bi_dtm <- dfm(toks_2gram) %>%
 
 bigram_keyness <- bi_dtm %>% textstat_keyness(target=1, measure="chi2")
 
-<<<<<<< HEAD
 bigram_keyness %>% write_csv("data/output/bigramms_1418_wojobbik.csv")
-=======
-bigram_keyness %>% write_csv("data/output/bigramms_1820.csv")
-
-# create wordcloud comparison
-
-textplot_wordcloud(dfm_group(bi_dtm, 'side'), comparison = TRUE,max_words = 100)
-
-dev.off()
->>>>>>> 80c4926aeb252dc88c571ade4a57fe998808b7ad
 
 textplot_wordcloud(dfm_group(bi_dtm, groups = side), comparison = TRUE,max_words = 100)
 
@@ -156,11 +124,7 @@ dtm_3gram <- dfm(toks_3gram) %>%
 
 trigram_keyness <- dtm_3gram %>% textstat_keyness(target=1,measure="chi2") 
 
-<<<<<<< HEAD
 trigram_keyness %>% write_csv("data/output/trigramms_1418_wojobbik.csv")
-=======
-trigram_keyness %>% write_csv("data/output/trigramms_1820.csv")
->>>>>>> 80c4926aeb252dc88c571ade4a57fe998808b7ad
 
 wordplot <- textplot_keyness(trigram_keyness,n=30,min_count = 5,margin=0.15)
 
@@ -173,26 +137,13 @@ dfm_group(dtm_3gram, groups = side) %>%
 
 # Create final n=2000 phrase list
 
-<<<<<<< HEAD
 bigrams <- read_csv("data/output/bigramms_1418_wojobbik.csv") %>%
   mutate(feature = str_replace_all(feature,"_"," "))
 trigrams <- read_csv("data/output/trigramms_1418_wojobbik.csv") %>%
   mutate(feature = str_replace_all(feature,"_"," "))
-=======
-unigrams <- read_csv("data/output/unigramms_1820.csv") %>% mutate(feature = str_replace_all(feature,"_"," "))
-bigrams <- read_csv("data/output/bigramms_1820.csv") %>% mutate(feature = str_replace_all(feature,"_"," "))
-trigrams <- read_csv("data/output/trigramms_1820.csv") %>% mutate(feature = str_replace_all(feature,"_"," "))
 
-unigrams <- read_csv("data/output/unigramms.csv") %>% mutate(feature = str_replace_all(feature,"_"," "))
-bigrams <- read_csv("data/output/bigramms.csv") %>% mutate(feature = str_replace_all(feature,"_"," "))
-trigrams <- read_csv("data/output/trigramms.csv") %>% mutate(feature = str_replace_all(feature,"_"," "))
->>>>>>> 80c4926aeb252dc88c571ade4a57fe998808b7ad
-
-unigrams <-  rbind(bigrams %>% head(500),unigrams %>% tail(500))
 bigrams <-  rbind(bigrams %>% head(500),bigrams %>% tail(500))
 trigrams <-  rbind(trigrams %>% head(500),trigrams %>% tail(500))
-
-p <- data.frame(cbind(c(unigrams$feature,bigrams$feature,trigrams$feature)))
 
 p <- data.frame(cbind(c(bigrams$feature,trigrams$feature)))
 
@@ -201,51 +152,22 @@ p$p <- str_replace_all(p$p," ","_")
 
 selected_ps <-  prep_stopwords(p %>% select(p))
 
-<<<<<<< HEAD
 selected_ps %>% write_rds("data/output/selected_parl_phrases_1418_wojobbik.rds")
 
 # Create phrase frequencies of selected phrases in parltext
 
 selected_ps <- read_rds("data/output/selected_parl_phrases_1418_wojobbik.rds")
-=======
-selected_ps %>% write_rds("data/output/selected_parl_phrases_1820.rds")
-selected_ps %>% write_rds("data/output/selected_parl_phrases_1920_wuni.rds")
-selected_ps %>% write_rds("data/output/selected_parl_phrases_1820_wuni.rds")
-
-# Create phrase frequencies of selected phrases in parltext
-
-selected_ps <- read_rds("data/output/selected_parl_phrases_1820.rds")
->>>>>>> 80c4926aeb252dc88c571ade4a57fe998808b7ad
 
 selected_parl_tokens <- parl_tokens %>% tokens_ngrams(n=2:3) %>%
   tokens_select(pattern = phrase(selected_ps), selection = "keep")
 
-<<<<<<< HEAD
-=======
-# phrase_frequency_table_parliament <- dfm(selected_parl_tokens, groups = "label")
-phrase_frequency_table_parliament <- dfm(selected_parl_tokens)
->>>>>>> 80c4926aeb252dc88c571ade4a57fe998808b7ad
 
 phrase_frequency_table_parliament <- dfm(selected_parl_tokens)
 
 # train wordscore model
 tmod_ws <- textmodel_wordscores(phrase_frequency_table_parliament, 
-<<<<<<< HEAD
                                 y = phrase_frequency_table_parliament$label,
                                 smooth = 0)
 summary(tmod_ws)
 
 tmod_ws %>% write_rds("data/output/wordscore_fit_1418_wojobbik.rds")
-=======
-                                y = phrase_frequency_table_parliament$label, smooth = 0)
-summary(tmod_ws)
-
-tmod_ws %>% write_rds("data/output/wordscore_fit_1820_pred_speaker_smmoth.rds")
-
-tmod_ws_old <- read_rds("data/output/wordscore_fit_1920_old.rds")
-
-summary(tmod_ws_old)
-
-
-rm(list=ls())
->>>>>>> 80c4926aeb252dc88c571ade4a57fe998808b7ad
