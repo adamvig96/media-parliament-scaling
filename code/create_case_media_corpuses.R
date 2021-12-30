f77967f30d75bd6f60690816da46f1d9fe671295
adamvig96 <vig1396@gmail.com>
2021-12-30 19:16:25 +0100

    add create_case_media.R

---
+++ code/create_case_media_corpuses.R
@@ -0,0 +1,30 @@
+renv::activate()
+rm(list=ls())
+
+library(dplyr)
+library(tidyverse)
+
+# Read raw
+media_corpus <- read_csv("data/raw/media_corpus_raw.csv") %>% 
+  mutate(site_month = substr(date, 1, 7 ))
+
+# Create case study corpuses
+index_corpus <- media_corpus %>% filter(date >= "2019-01-01")
+corpus <- corpus(index_corpus$content)
+docvars(corpus, "page") <- index_corpus$page
+docvars(corpus, "site_month") <- index_corpus$site_month
+write_rds(corpus, "data/output/index_case_corpus.rds")
+
+origo_corpus <- media_corpus %>% filter(date >= "2014-01-01") %>% 
+  filter(date < "2018-01-01")
+corpus <- corpus(origo_corpus$content)
+docvars(corpus, "page") <- origo_corpus$page
+docvars(corpus, "site_month") <- origo_corpus$site_month
+write_rds(corpus, "data/output/origo_case_corpus.rds")
+
+mno_corpus <- media_corpus %>% filter(date >= "2013-01-01") %>% 
+  filter(date < "2017-01-01")
+corpus <- corpus(mno_corpus$content)
+docvars(corpus, "page") <- mno_corpus$page
+docvars(corpus, "site_month") <- mno_corpus$site_month
+write_rds(corpus, "data/output/mno_case_corpus.rds")
