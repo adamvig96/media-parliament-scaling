07f1589807cfe62b6edacd1ec5a4fd58d30e6825
adamvig96 <vig1396@gmail.com>
2022-01-01 17:26:52 +0100

    modify to create year samples

--- code/create_case_media_corpuses.R
+++ code/create_case_media_corpuses.R
@@ -8,26 +8,17 @@ library(quanteda)
 # Read raw
 media_corpus <- read_csv("data/raw/media_corpus_raw.csv") %>% 
   mutate(ym = substr(date, 1, 7 )) %>% 
-  mutate(site_month = paste(page, ym, sep="_"))
+  mutate(site_month = paste(page, ym, sep="_")) %>% 
+  mutate(year = as.integer(substr(date, 1, 4)))
 
-# Create case study corpuses
-index_corpus <- media_corpus %>% filter(date >= "2019-01-01")
-corpus <- corpus(index_corpus$content)
-docvars(corpus, "page") <- index_corpus$page
-docvars(corpus, "site_month") <- index_corpus$site_month
-write_rds(corpus, "data/output/index_case_corpus.rds")
+# Create year sample corpuses
 
-origo_corpus <- media_corpus %>% filter(date >= "2014-01-01") %>% 
-  filter(date < "2018-01-01")
-corpus <- corpus(origo_corpus$content)
-docvars(corpus, "page") <- origo_corpus$page
-docvars(corpus, "site_month") <- origo_corpus$site_month
-write_rds(corpus, "data/output/origo_case_corpus.rds")
-
-mno_corpus <- media_corpus %>% filter(date >= "2013-01-01") %>% 
-  filter(date < "2017-01-01")
-corpus <- corpus(mno_corpus$content)
-docvars(corpus, "page") <- mno_corpus$page
-docvars(corpus, "site_month") <- mno_corpus$site_month
-write_rds(corpus, "data/output/mno_case_corpus.rds")
-
+for (year_index in 2010:2021){
+  year_sample <- media_corpus %>% filter(year == year_index)
+  corpus <- corpus(year_sample$content)
+  docvars(corpus, "page") <- year_sample$page
+  docvars(corpus, "site_month") <- year_sample$site_month
+  write_rds(corpus,
+            paste("data/output/media_corpus_", as.character(year_index), ".rds",
+                  sep = ""))
+}
