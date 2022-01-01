366b04f0be1a09f710c92fae0ed4d7ce0f769df3
adamvig96 <vig1396@gmail.com>
2022-01-01 17:27:24 +0100

    predict slant by year

--- code/predict_media_slant.R
+++ code/predict_media_slant.R
@@ -1,8 +1,8 @@
-###########################################################################################
+################################################################################
 
 # Predict slant for media outlets with previously trained wordscore model
 
-###########################################################################################
+################################################################################
 
 
 rm(list=ls())
@@ -19,28 +19,37 @@ library(tidyverse)
 
 tmod_ws <-  read_rds("data/output/wordscore_fit.rds")
 selected_phrases <- read_rds("data/output/selected_phrases.rds")
-corpus <- read_rds("data/output/index_case_corpus.rds")
 
-media_tokens <- tokens(corpus, 
-                       remove_punct = T,
-                       remove_symbols = T,
-                       remove_numbers = T,
-                       remove_separators = T) %>% 
-                tokens_tolower() %>%
-                tokens_wordstem(language = 'hu') %>%
-                tokens_ngrams(n = 2:3) %>%
-                tokens_select(pattern = selected_phrases, selection = "keep") 
-
-rm(corpus)
-
-phrase_frequency_table_media <- dfm(media_tokens) %>% dfm_group(groups = site_month)
-
-# predict slant
-
-pred_ws <- predict(tmod_ws, se.fit = TRUE, newdata = phrase_frequency_table_media)
-
-predicted_slant <- as.data.frame(pred_ws) 
-predicted_slant <- cbind(site_month = rownames(predicted_slant), predicted_slant)
-rownames(predicted_slant) <- 1:nrow(predicted_slant)
-
-predicted_slant %>% write_csv("data/output/monthly_slant_pred.csv")
+for (year_index in 2010:2021){
+  corpus <- read_rds(
+    paste("data/output/media_corpus_", as.character(year_index), ".rds",
+                           sep = "")
+    )
+  
+  media_tokens <- tokens(corpus, 
+                         remove_punct = T,
+                         remove_symbols = T,
+                         remove_numbers = T,
+                         remove_separators = T) %>% 
+    tokens_tolower() %>%
+    tokens_wordstem(language = 'hu') %>%
+    tokens_ngrams(n = 2:3) %>%
+    tokens_select(pattern = selected_phrases, selection = "keep") 
+  
+  rm(corpus)
+  
+  phrase_frequency_table_media <- dfm(media_tokens) %>%
+    dfm_group(groups = site_month)
+  
+  # predict slant
+  
+  pred_ws <- predict(tmod_ws, se.fit = TRUE, newdata = phrase_frequency_table_media)
+  
+  predicted_slant <- as.data.frame(pred_ws) 
+  predicted_slant <- cbind(site_month = rownames(predicted_slant), predicted_slant)
+  rownames(predicted_slant) <- 1:nrow(predicted_slant)
+  
+  predicted_slant %>% write_csv(
+    paste("data/output/monthly_slant_pred_", as.character(year_index), ".csv", sep = "")
+    )
+}
