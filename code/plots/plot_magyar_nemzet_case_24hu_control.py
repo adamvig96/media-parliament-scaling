8c6a5fdfdbeda35b8822a2cbee563a6d9baa8c0c
adamvig96 <vig1396@gmail.com>
2022-01-10 00:28:21 +0100

    add 24.hu as control

---
+++ code/plots/plot_magyar_nemzet_case_24hu_control.py
@@ -0,0 +1,68 @@
+import datetime as dt
+import sys
+import warnings
+
+import matplotlib.patches as mpatches
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+warnings.filterwarnings("ignore")
+sys.path.append("code/plots")
+
+from plot_helper_functions import *
+
+df = execute_formating().loc[
+    lambda x: x["site"].isin(["24.hu", "mno.hu", "888.hu", "mno.hu/magyaridok.hu"])
+]
+figname = "slant_estimates_magyar_nemzet_case_24hu_control.png"
+
+plt.figure(figsize=(10, 7))
+sns.set_theme(style="whitegrid")
+colors = ["#474755", "#e01164", "#133c5c", "#133c5c"]
+sns.set_palette(sns.color_palette(colors))
+
+sns.lineplot(x="date", y="slant", hue="site", style="variable", data=df)
+
+
+nyolcas = mpatches.Patch(color=colors[0], label="888.hu")
+index = mpatches.Patch(color=colors[1], label="24.hu")
+mno = mpatches.Patch(color=colors[2], label="mno.hu")
+
+plt.legend(
+    handles=[nyolcas, index, mno],
+    loc=0,
+    borderaxespad=1.0,
+    frameon=False,
+    title=False,
+    numpoints=3,
+)
+plt.title("Online hírportálok torzítottsága:\n a Magyar Nemzet esete", size=20, y=1.03)
+plt.ylabel("Becsült torzítottság")
+plt.xlabel(None)
+plt.ylim(0.4, 0.65)
+
+# G nap
+plt.axvline(dt.datetime(2015, 2, 6), color="#000000")
+plt.annotate(
+    "G-nap",
+    xy=(3, 1),
+    xycoords="axes fraction",
+    xytext=(0.38, 0.83),
+    textcoords="axes fraction",
+)
+plt.annotate(
+    "Simicska\n bezár",
+    xy=(3, 1),
+    xycoords="axes fraction",
+    xytext=(0.65, 0.15),
+    textcoords="axes fraction",
+)
+plt.annotate(
+    "Magyar Idők nevet vált\nMagyar Nemzetre",
+    xy=(3, 1),
+    xycoords="axes fraction",
+    xytext=(0.6, 0.59),
+    textcoords="axes fraction",
+)
+
+plt.savefig("figures/" + figname, dpi=1000)
